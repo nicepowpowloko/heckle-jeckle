b566308134f2d4dc86d97ef148a34ae6924c5282
nicepowpowloko <thomas22751@gmail.com>
2021-11-23 10:13:49 +0100

    23/11

--- acceuil/en-tête.php
+++ acceuil/en-tête.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>en-tête</title>
 </head>
 <body>
 
 <header>
-    <img src="../heckle-jeckle/Le Bazar de Heckle et Jeckle/Autres/Header.png" alt="">
+    
 </header>
     
 </body>
