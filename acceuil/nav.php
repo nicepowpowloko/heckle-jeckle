d6408dd815df46ebd56b40afb4c38fa85f634a29
Lorenzo <codispoti.lorenzo.pro@outlook.fr>
2021-12-02 11:32:38 +0100

    Rajout Logo connecter menu

--- acceuil/nav.php
+++ acceuil/nav.php
@@ -5,6 +5,7 @@
             <a href="../Page du Site/vêtements & accesoires.php">VETEMENTS & ACCESSOIRES</a>
             <a href="../Page du Site/divers.php">DIVERS</a>
             <a href="">PANIER</a>
+            <a href=""><img src="../Images/kissclipart-emirati-man-cartoon-clipart-emiratis-dubai-man-051efe8dc99e1f0a.png" height="50px"></a>
           </div> 
 </nav>
 
@@ -20,6 +21,7 @@
     display: flex;
     font-family: 'Arabela';
     flex-wrap: wrap;
+    align-items: center;
 }
   
 .topnav a{
